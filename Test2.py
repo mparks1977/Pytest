80098fe1686b954f42929f9ba84353cf6409fd71
Michael Parks <mparks.mail@gmail.com>
2020-10-20 09:41:05 -0400

    New Code

---
+++ Test2.py
@@ -0,0 +1,22 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+class Photography:
+    def __init__(self, camera, shots):
+        self.camera = camera
+        self.shots = shots
+
+    def myfunc(self):
+        print("My camera is a " + self.camera)
+        
+p1 = Photography("Nikon", 52)
+p1.myfunc()
